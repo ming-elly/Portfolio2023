c78043409967f7127d5c455ebc245a73d13d5324
min.elly <achachan20010920@gmail.com>
2023-04-27 15:04:53 +0900

    ft add

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.slider').slick({
+    $('.slider-1').slick({
       autoplay: true,
       autoplaySpeed: 4000, // 画像の表示時間を4秒に設定
       arrows: false, // 左右の矢印ボタンを非表示にする
@@ -7,5 +7,15 @@ $(document).ready(function(){
       fade: true,
       speed: 850
     });
+
+    $('.slider-2').slick({
+      autoplay: true,
+      autoplaySpeed: 4000, // 画像の表示時間を4秒に設定
+      arrows: false, // 左右の矢印ボタンを非表示にする
+      infinite: true, // スライドが最後まで行ったら最初に戻る（デフォルト設定）
+      fade: true,
+      speed: 850
+    });
+
   });
   
